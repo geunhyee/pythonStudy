5bb95775ff9ae1cae0ae8d90c01cbf3310574402
geunhyee <147581668+geunhyee@users.noreply.github.com>
2023-11-07 17:19:14 +0900

    Add files via upload

---
+++ flask-web/static/coin.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(document).on('click', 'tr', function(){
+        alert($(this).text())
+        let code =$(this).find('td').eq(2).text();
+        let subUrl = "http://127.0.0.1:5555/coin";
+        $.ajax({
+            url : subUrl
+            ,type : 'POST'
+            ,data : JSON.stringify({'market':code})
+            ,dataType:'json'
+            ,success:function(res){
+                console.log(res);
+                alert(res[0]['high_price']);
+
+            }
+            ,error(e){
+                console.log(e);
+            }
+        })
+    });
+
+});
